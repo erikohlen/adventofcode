d419721dc802db52da87d911bf00f940d4aa72de
Erik <hoogalei@gmail.com>
2022-12-05 20:58:27 +0100

    Cleaned the stacks

--- adventofcode/lib/2022/dec5_2022.dart
+++ adventofcode/lib/2022/dec5_2022.dart
@@ -1,7 +1,7 @@
 import 'package:adventofcode/utils/load_utils.dart';
 import 'package:flutter/material.dart';
 
-const day = 4;
+const day = 5;
 
 class Dec5_2022 extends StatefulWidget {
   const Dec5_2022({super.key});
@@ -11,7 +11,6 @@ class Dec5_2022 extends StatefulWidget {
 
 class _Dec5_2022State extends State<Dec5_2022> {
   String sampleStr = "";
-  String sample2Str = "";
   String inputStr = "";
 
   @override
@@ -21,11 +20,7 @@ class _Dec5_2022State extends State<Dec5_2022> {
         sampleStr = value;
       });
     });
-    loadAssetFileAsString("2022_${day}_sample2.txt").then((value) {
-      setState(() {
-        sample2Str = value;
-      });
-    });
+
     loadAssetFileAsString("2022_${day}_input.txt").then((value) {
       setState(() {
         inputStr = value;
@@ -36,13 +31,48 @@ class _Dec5_2022State extends State<Dec5_2022> {
 
   @override
   Widget build(BuildContext context) {
-    var sample = inputStr.split('\n').map((e) => e.split('\n')).toList();
+    var sample = sampleStr;
     var outputs = <String>[];
     void addToOutput(List<String> list, String varName, dynamic val) {
       list.add('$varName: $val');
     }
 
-    var sample2 = sample.map((e) => e[0].split(',')).toList();
+    // Get sample but only include row 1 to 9
+    var sampleRows = sample.split('\n');
+
+    // Number of stacks
+    var bottomRowIndex = 7; // Add this manually by looking at input
+    var widthMax = sampleRows[bottomRowIndex].length;
+    var numStack = (widthMax + 1) / 4;
+    addToOutput(outputs, 'numStack', numStack);
+
+    // Create list of letters for each stack
+    var heightRangeStart = 0;
+    var heightRangeEnd = bottomRowIndex;
+    var heightRange = heightRangeEnd - heightRangeStart + 1;
+    addToOutput(outputs, 'heightRange', heightRange);
+
+    var startColumn = 0;
+    var stacks = [];
+    for (var i = 0; i < numStack; i++) {
+      List<String> _stack = [];
+      for (var j = 0; j < heightRange; j++) {
+        var row = sampleRows[j];
+        var letter = row[startColumn + 1];
+        _stack.add(letter);
+      }
+      stacks.add(_stack);
+
+      startColumn += 4;
+    }
+    // Remove empty strings from stacks
+    stacks.forEach((element) {
+      element.removeWhere((element) => element == ' ');
+    });
+
+    stacks.forEach((element) {
+      addToOutput(outputs, 'stack', element);
+    });
 
     //! PART 1
 
