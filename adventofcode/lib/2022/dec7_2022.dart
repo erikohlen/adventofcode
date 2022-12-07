b9d7a6c731252baefb530aa8d020e57ed4005143
Erik <hoogalei@gmail.com>
2022-12-08 00:40:34 +0100

    Solved dec7

--- adventofcode/lib/2022/dec7_2022.dart
+++ adventofcode/lib/2022/dec7_2022.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 
 const day = 7;
 
+enum RowType { cdDir, cdDotDot, ls, dir, file }
+
+/* 
+    - / (dir)
+      - a (dir)
+        - e (dir)
+          - i (file, size=584)
+          - f (file, size=29116)
+          - g (file, size=2557)
+          - h.lst (file, size=62596)
+    - b.txt (file, size=14848514)
+    - c.dat (file, size=8504156)
+    - d (dir)
+      - j (file, size=4060174)
+      - d.log (file, size=8033020)
+      - d.ext (file, size=5626152)
+      - k (file, size=7214296) 
+    */
+
 class Dec7_2022 extends StatefulWidget {
   const Dec7_2022({super.key});
   @override
@@ -41,72 +60,95 @@ class _Dec7_2022State extends State<Dec7_2022> {
       return Text('loading...');
     }
 
-    //! PARSING
-    var lines = sample.split('\n');
+    var rows = sample.split('\n');
 
-    var dirs = sample.split('\$ ls');
-
-    // Sum size of current folder
-    int sizeOfCurrentFolder(String e) {
-      var lines = e.split('\n');
-      var sum = 0;
-      lines.forEach((element) {
-        // If string contains a number
-        if (RegExp(r'\d').hasMatch(element)) {
-          var size = int.parse(element.split(' ')[0]);
-          sum += size;
-        }
-      });
-
-      return sum;
+    RowType strToType(String str) {
+      if (str.startsWith('\$ cd ..')) {
+        return RowType.cdDotDot;
+      } else if (str.startsWith('\$ cd')) {
+        return RowType.cdDir;
+      } else if (str.startsWith('\$ ls')) {
+        return RowType.ls;
+      } else if (str.startsWith('dir')) {
+        return RowType.dir;
+      } else if (str.startsWith(RegExp(r'[0-9]'))) {
+        return RowType.file;
+      } else {
+        throw Exception('Unknown row type: $str');
+      }
     }
 
-    dirs.forEach((element) {
-      addToOutput(outputs, 'element', element);
-      addToOutput(outputs, 'size', sizeOfCurrentFolder(element));
+    Map<String, int> dirMap = {};
+    String currentDir = '/';
+    List<String> alreadyAddedFiles = [];
+    rows.forEach((element) {
+      var type = strToType(element);
+
+      switch (type) {
+        case RowType.cdDir:
+          if (currentDir == '/') {
+            currentDir += element.split(' ')[2];
+          } else {
+            currentDir += '/${element.split(' ')[2]}';
+          }
+          break;
+        case RowType.cdDotDot:
+          if (currentDir != '/') {
+            var currentDirParts = currentDir.split('/');
+            currentDirParts.removeLast();
+            currentDir = currentDirParts.join('/');
+          }
+          break;
+        case RowType.ls:
+          break;
+        case RowType.dir:
+          break;
+        case RowType.file:
+          String fileName = element.split(' ')[1];
+          if (alreadyAddedFiles.contains(currentDir + '/' + fileName)) {
+            break;
+          }
+          int fileSize = int.parse(element.split(' ')[0]);
+
+          dirMap[currentDir] = (dirMap[currentDir] ?? 0) + fileSize;
+          var parentDirs = currentDir.split('/');
+
+          for (var i = 0; i < parentDirs.length; i++) {
+            var parentDir = parentDirs.sublist(0, i).join('/');
+            if (parentDir != '') {
+              dirMap[parentDir] = (dirMap[parentDir] ?? 0) + fileSize;
+            }
+          }
+
+          if (currentDir != '/') {
+            dirMap['/'] = (dirMap['/'] ?? 0) + fileSize;
+          }
+          alreadyAddedFiles.add(currentDir + '/' + fileName);
+          break;
+
+        default:
+      }
     });
 
-    /*  var sumsPerDir = dirs.map((e) {
-      var lines = e.split('\n');
-      var sum = 0;
-      lines.forEach((element) {
-        // If string contains a number
-        if (RegExp(r'\d').hasMatch(element)) {
-          var size = int.parse(element.split(' ')[0]);
-          sum += size;
-        }
-      });
+    var sumSizePart1 = 0;
+    dirMap.keys.forEach((key) {
+      int fileSize = dirMap[key]!;
+      if (fileSize <= 100000) {
+        sumSizePart1 += fileSize;
+      }
+    });
 
-      return sum;
-    }); */
+    var availableSpace = 70000000 - dirMap['/']!;
+    var spaceToDelete = 30000000 - availableSpace;
+    addToOutput(outputs, 'Part1: sumSize', sumSizePart1);
 
-    // Filter sumsPerDir to only include numbers of max 100000
-    //sumsPerDir = sumsPerDir.where((element) => element <= 100000);
-    //var sumUpto100000 = sumsPerDir.reduce((value, element) => value + element);
-    // addToOutput(outputs, 'sumsPerDir', sumUpto100000);
+    var sortedSizes = dirMap.values.toList();
+    sortedSizes.sort();
+    var minToDelete =
+        sortedSizes.firstWhere((element) => element >= spaceToDelete);
+    addToOutput(outputs, 'Part2: minToDelete', minToDelete);
 
-    /*  sumsPerDir.forEach((element) {
-      addToOutput(outputs, 'sum: ', element);
-    }); */
-    /* 
-    - / (dir)
-      - a (dir)
-        - e (dir)
-          - i (file, size=584)
-          - f (file, size=29116)
-          - g (file, size=2557)
-          - h.lst (file, size=62596)
-    - b.txt (file, size=14848514)
-    - c.dat (file, size=8504156)
-    - d (dir)
-      - j (file, size=4060174)
-      - d.log (file, size=8033020)
-      - d.ext (file, size=5626152)
-      - k (file, size=7214296) 
-    */
-
-    //! PART 1
-
+    // Final 3979145
     return Padding(
       padding: const EdgeInsets.all(32.0),
       child: ListView(
